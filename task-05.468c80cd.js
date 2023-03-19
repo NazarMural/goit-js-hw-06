2c2cbc473b349352139a7d1197035dea07556d47
NazarMural <61448859+NazarMural@users.noreply.github.com>
2023-03-19 21:57:14 +0000

    Deploying to gh-pages from @ NazarMural/goit-js-hw-06@b34a5e8f6374b3ede9e2047cce5df1277568588c 🚀

---
+++ task-05.468c80cd.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("#name-input"),e=document.querySelector("#name-output ");t.addEventListener("input",(function(){var n=t.value.trim()||"Anonymous";e.textContent=n}))}();
+//# sourceMappingURL=task-05.468c80cd.js.map
---
+++ task-05.eb6286f3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("#name-input"),e=document.querySelector("#name-output ");t.addEventListener("input",(function(){const n=t.value.trim()||"Anonymous";e.textContent=n}));
+//# sourceMappingURL=task-05.eb6286f3.js.map
